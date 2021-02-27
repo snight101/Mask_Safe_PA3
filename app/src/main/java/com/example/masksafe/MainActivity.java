1184d9b8c5815eb80775cdb7cc0afea9f3b1dd70
snight <snight@iu.edu>
2021-02-26 20:12:35 -0500

    First commit

---
+++ app/src/main/java/com/example/masksafe/MainActivity.java
@@ -0,0 +1,104 @@
+package com.example.masksafe;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    //Used an Image button to take place of a Google Maps API
+    private ImageButton mImageButton;
+    private static final boolean USE_FLAG = true;
+    private static final int mFlag = Intent.FLAG_ACTIVITY_REORDER_TO_FRONT;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mImageButton = (ImageButton)findViewById(R.id.imageButton);
+
+        ActionBar AB = getSupportActionBar();
+        AB.setTitle("Mask Safe");
+        // Define ColorDrawable object and parse color
+        // using parseColor method
+        // with color hash code as its parameter
+        ColorDrawable colorDrawable
+                = new ColorDrawable(Color.parseColor("#2F9CCD"));
+
+        // Set BackgroundDrawable
+        AB.setBackgroundDrawable(colorDrawable);
+
+        AB.setDisplayShowHomeEnabled(true);
+        AB.setDisplayUseLogoEnabled(true);
+        AB.setLogo(R.drawable.ic_logo);
+    }
+
+    /*
+
+       Adding Intent and flag commands
+
+     */
+
+    @Override
+    protected  void onNewIntent(Intent intent){
+        super.onNewIntent(intent);
+        setIntent(intent);
+    }
+    public void onImageClick(View v){
+        Intent myIntent = new Intent(this, SamplePage.class);
+        if(USE_FLAG){
+            myIntent.addFlags(mFlag);
+        }
+        startActivity(myIntent);
+    }
+
+
+
+
+
+
+    /*
+
+        Adding menu to page
+
+     */
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.menu_mask_safe, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        // Menu Commands
+        // works like an if else statement
+        switch (id) {
+            case R.id.profileButton:
+                Intent myIntent = new Intent(this, AccountPage.class);
+                if(USE_FLAG){
+                    myIntent.addFlags(mFlag);
+                }
+                startActivity(myIntent);
+                return true;
+            case R.id.mapButton:
+                Intent myIntent2 = new Intent(this, MainActivity.class);
+                if(USE_FLAG){
+                    myIntent2.addFlags(mFlag);
+                }
+                startActivity(myIntent2);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+}
---
+++ app/src/main/java/com/example/masksafe/SamplePage.java
@@ -0,0 +1,85 @@
+package com.example.masksafe;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageButton;
+
+public class SamplePage extends AppCompatActivity {
+
+    private static final boolean USE_FLAG = true;
+    private static final int mFlag = Intent.FLAG_ACTIVITY_REORDER_TO_FRONT;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sample_page);
+
+        ActionBar AB = getSupportActionBar();
+        AB.setTitle("Mask Safe");
+        // Define ColorDrawable object and parse color
+        // using parseColor method
+        // with color hash code as its parameter
+        ColorDrawable colorDrawable
+                = new ColorDrawable(Color.parseColor("#2F9CCD"));
+
+        // Set BackgroundDrawable
+        AB.setBackgroundDrawable(colorDrawable);
+
+        AB.setDisplayShowHomeEnabled(true);
+        AB.setDisplayUseLogoEnabled(true);
+        AB.setLogo(R.drawable.ic_logo);
+    }
+
+    /*
+
+        Adding menu to page
+
+     */
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.menu_mask_safe, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        // works like an if else statement
+        switch (id) {
+            case R.id.profileButton:
+                Intent myIntent = new Intent(this, AccountPage.class);
+                if(USE_FLAG){
+                    myIntent.addFlags(mFlag);
+                }
+                startActivity(myIntent);
+                return true;
+            case R.id.mapButton:
+                Intent myIntent2 = new Intent(this, MainActivity.class);
+                if(USE_FLAG){
+                    myIntent2.addFlags(mFlag);
+                }
+                startActivity(myIntent2);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    public void submitReviewButtonClick(View v){
+        Intent myIntent = new Intent(this, SubmitReview.class);
+        if(USE_FLAG){
+            myIntent.addFlags(mFlag);
+        }
+        startActivity(myIntent);
+    }
+
+
+}
